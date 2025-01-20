d8501607bc58ffe343c827f31644ec03372de8e1
Avishek Prasad <avishekprasad0999@gmail.com>
2025-01-20 22:26:35 +0530

    fix repeting array

--- avishek/array/class2/shortOne.cpp
+++ avishek/array/class2/shortOne.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 void shortFunction(int arr[], int size) {
-    int zero_count = 0; // Counter for zeros
-    int one_count = 0;  // Counter for ones
+    int zero_count = 0;
+    int one_count = 0;
 
-    // Counting zeros and ones
     for (int i = 0; i < size; i++) {
         if (arr[i] == 0) {
             zero_count++;
@@ -17,17 +16,14 @@ void shortFunction(int arr[], int size) {
 
     int i = 0;
 
-    // Filling the array with zeros
     for (; i < zero_count; i++) {
         arr[i] = 0;
     }
 
-    // Filling the array with ones
     for (; i < size; i++) {
         arr[i] = 1;
     }
 
-    // Printing the array after sorting
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
@@ -37,7 +33,7 @@ int main() {
     int arr[] = {0, 1, 0, 1, 0, 0, 0, 1};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    shortFunction(arr, size);  // Call the shortFunction
+    shortFunction(arr, size);
 
     return 0;
 }
