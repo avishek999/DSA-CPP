ed74a501bcd8bd8be158a79e71adb237a1efccdd
Avishek Prasad <avishekprasad0999@gmail.com>
2025-01-21 18:21:35 +0530

    did more question

--- avishek/array/class2/shortOne.cpp
+++ avishek/array/class2/shortOne.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
 using namespace std;
 
-void shortFunction(int arr[], int size) {
-    int zero_count = 0;
-    int one_count = 0;
 
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == 0) {
-            zero_count++;
-        }
-        else if (arr[i] == 1) {
-            one_count++;
-        }
-    }
 
-    int i = 0;
+void shortZeroOne(int arr[],int size){
 
-    for (; i < zero_count; i++) {
-        arr[i] = 0;
-    }
+  int zero =0;
+  int one =0;
 
-    for (; i < size; i++) {
-        arr[i] = 1;
-    }
+  for(int i=0;i<size;i++){
+  if(arr[i] == 0){
+    zero++;
+  }
+ else if(arr[i] == 1){
+    one++;
+  }
+  }
 
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
+  int i=0;
+
+  for(;i<zero;i++){
+    arr[i] = 0;
+  }
+  for(;i<size;i++){
+    arr[i] = 1;
+  }
 }
 
+
 int main() {
-    int arr[] = {0, 1, 0, 1, 0, 0, 0, 1};
-    int size = sizeof(arr) / sizeof(arr[0]);
+  int arr[] = {0,1,1,1,0,0,0};
+  int size = 7;
 
-    shortFunction(arr, size);
+  shortZeroOne(arr,size);
 
-    return 0;
+  for(int i=0;i<size;i++){
+    cout<<arr[i];
+  }
+  return 0;
 }
