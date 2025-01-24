2b27b2bb4badafdd7a7a213afcaa501eb8dcda0a
Simran Kumari <avishekprsad0999@gmail.com>
2025-01-24 17:46:52 +0530

    added some stuff

--- avishek/array/class2/3some.cpp
+++ avishek/array/class2/3some.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        
+    vector<vector<int>> threeSum(vector<int>& nums) {
 
-        for(int i = 0 ;i < nums.size(); i++ ){
-         for(int j = i+1 ; j < nums.size() ; j++){
-            if( nums[i] + nums[j] == target){
-                return {i , j};
+        vector<vector<int>> ans;
+
+        for(int i = 0 ;i < nums.size();i++){
+            for(int j = i+1 ; j < nums.size();j++){
+            for(int k = j+1 ; k < nums.size();k++){
+            
+           if(nums[i] + nums[j] + nums[k] == 0){
+            vector<int> temp;
+            temp.push_back(nums[i]);
+            temp.push_back(nums[j]);
+            temp.push_back(nums[k]);
+            ans.push_back(temp);
+          
+
             }
-         }
+            }
+            }
         }
-        return {};
+        return ans;
+        
     }
 };
