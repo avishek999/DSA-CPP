1fb14a1e5cf0155286c91081363a2781b37bb3e9
Avishek Prasad <avishekprasad0999@gmail.com>
2025-01-25 11:18:48 +0530

    added new rotate an array question

--- avishek/array/class2/3some.cpp
+++ avishek/array/class2/3some.cpp
@@ -1,3 +1,7 @@
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -16,9 +20,22 @@ public:
             ans.push_back(temp);
           
 
+<<<<<<< HEAD
             }
             }
             }
+=======
+        for(int i = 0 ;i < nums.size(); i++ ){
+         for(int j = i+1 ; j < nums.size() ; j++){
+         for(int k = j+1 ; k < nums.size() ; k++){
+
+            if( nums[i] + nums[j]  + nums[k]== target){
+                return {i , j ,k};
+            }
+
+         }
+         }
+>>>>>>> cb2d8ff (added some stuff)
         }
         return ans;
         
---
+++ avishek/array/class2/rotateArray.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+
+void rotate(int arr[], int n, int d){
+
+// if the size of array is bigger then the shift then we can use the modulo operator to get the final shift
+  int finalShift = d % n;
+
+// if the final shift is  0  then we dont need to do the operation we can just send the same array
+ if(finalShift == 0){
+    return;
+ }
+
+    // step1 : store the dth element in a temp array
+    int temp[1000];
+    int index  = 0;
+
+    for(int i = n-finalShift ; i < n ; i++){
+        temp[index] = arr[i];
+        index++;
+    }
+ // step2 : shift the array to the left by finalshift elements
+    for(int i = n-1; i >= 0; i--){
+        arr[i] = arr[i-finalShift];
+    }
+
+// step3 : copy the temp array to the original array
+
+    for(int i = 0; i < finalShift; i++){
+        arr[i] = temp[i];
+    }
+}
+
+
+int main()
+{
+   int arr[] = {1,2,3,4,5,6};
+   int n = sizeof(arr)/sizeof(arr[0]);
+   int d = 3;
+
+  rotate(arr,n,d);
+   for(int i=0; i<n; i++)
+   {
+    cout<<arr[i]<<" ";
+   }
+
+    return 0;
+}
