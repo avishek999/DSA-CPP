3635c4ca1907d0c2c70456371f2243e1a5c770ed
Avishek Prasad <avishekprasad0999@gmail.com>
2025-01-27 10:00:25 +0530

    added some code

---
+++ avishek/array/assignment/pivot.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int pivot(int arr[] , int size){
+
+   int l = 0;
+   int r = size-1;
+   int m = l + (r - l) / 2;
+
+
+}
+
+
+int main()
+{
+
+    int  nums[] = {1,7,3,6,5,6};
+    int size = 6;
+  
+
+  int value = pivot(nums,size);
+   
+    
+    return 0;
+}
